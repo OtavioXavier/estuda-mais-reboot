1f32120cb54bbc7af7d65ec3dbe4f86e94894384
OtavioXavier <otavioxavier.santos@ucsal.edu.br>
2025-01-09 00:42:57 -0300

    :recycle: refactor: removendo importação inutil

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,52 +1,52 @@
 'use server'
 import { createGoogleGenerativeAI } from '@ai-sdk/google';
-import { generateObject, generateText } from 'ai';
+import { generateObject } from 'ai';
 import { z } from 'zod';
 import { schemaSummaryQuestions } from '@/types/schemas';
 import { SQ } from '@/types';
 
 const generateSummarySchema = z.object({
-   assunto: z.string().min(2, { message: 'é necessário um assunto para começarmos os resumos' })
+  assunto: z.string().min(2, { message: 'é necessário um assunto para começarmos os resumos' })
 })
 
 export const generateSummary = async (_: unknown, data: FormData): Promise<SQ | string | null> => {
-   try {
-     const result = generateSummarySchema.safeParse(Object.fromEntries(data));
+  try {
+    const result = generateSummarySchema.safeParse(Object.fromEntries(data));
+
+    if (!result.success) {
+      return result.error.flatten().fieldErrors.assunto?.[0] ?? 'Erro de validação.';
+    }
+
+    const { assunto } = result.data;
+
+    const genAI = createGoogleGenerativeAI({
+      apiKey: process.env.NEXT_PUBLIC_GOOGLE_GENERATIVE_AI_API_KEY,
+    });
+
+    const prompt = `
+       Pesquise nos 5 melhores sites de noticias e artigos cientificos para este assunto: ${assunto}.
+       
+       Para cada site, forneça um resumo conciso e objetivo dos artigos e sites mais relevantes sobre ${assunto}. Os resumos devem conter o nome do site onde foi retirado o texto para resumo, mas basta o nome principal, como por exemplo, Wikipedia, um bom título, o texto pode ser longo com cerca de 600 palavras e um link para o texto original diretamente no site e ao todo devem ser 5 resumos. 
+
+       Após isto crie ou pegue da internet 10 a 15 questões relacionadas estes resumos.
  
-     if (!result.success) {
-       return result.error.flatten().fieldErrors.assunto?.[0] ?? 'Erro de validação.';
-     }
+       As questões devem ser divididas em abertas e objetivas, nas objetivas é necessário uma resposta correta (reposta), alternativas (array onde a primeira posição equivale a letra A e assim por diante).
  
-     const { assunto } = result.data;
- 
-     const genAI = createGoogleGenerativeAI({
-       apiKey: process.env.NEXT_PUBLIC_GOOGLE_GENERATIVE_AI_API_KEY,
-     });
- 
-     const prompt = `
-       Pesquise nos 6 melhores sites do google para este assunto: ${assunto}.
- 
-       Depois, liste um resumo para cada site contendo pelo menos 3 paragrafos com 600 caracteres.
-       Após isto crie ou pegue da internet 10 a 15 questões sobre estes resumos.
- 
-       As questões podem ser divididas em abertas e objetivas, nas objetivas é necessário uma resposta correta (reposta), alternativas (array onde a primeira posição equivale a letra A e assim por diante).
- 
-       Os resumos devem conter o nome do site resumido, mas basta o nome principal, como por exemplo, Wikipedia. Devem conter também parágrafos onde deve ser discorrido este resumo, onde, cada item no array parágrafos é respectivamente um parágrafo. Além de um bom título... Devem conter pelo menos 6 resumos.
- 
-       Tudo deve ter um id uuid seguindo o schema.
+       Todos os itens criados devem conter um id uuid e seguir o schema.
      `;
- 
-     const { object } = await generateObject({
-       model: genAI('gemini-1.5-flash-8b'),
-       schema: schemaSummaryQuestions,
-       prompt,
-     });
- 
-     const validatedObject = schemaSummaryQuestions.parse(object);
- 
-     return validatedObject;
-   } catch (error) {
-     console.error('Erro durante a geração de resumos e questões: ', error);
-     return null;
-   }
- };
+
+    const { object } = await generateObject({
+      model: genAI('gemini-2.0-flash-exp'),
+      schema: schemaSummaryQuestions,
+      prompt,
+    });
+    console.log('resumos: ', { object })
+
+    const validatedObject = schemaSummaryQuestions.parse(object);
+
+    return validatedObject;
+  } catch (error) {
+    console.error('Erro durante a geração de resumos e questões: ', error);
+    return null;
+  }
+};
