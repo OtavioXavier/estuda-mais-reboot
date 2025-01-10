ffe3fbfbd9c659c6ab71c5f285fe3b6c1baa704b
OtavioXavier <otavioxavier.santos@ucsal.edu.br>
2025-01-10 20:02:11 -0300

    :bug: fix: key prop em cada questão

--- components/resumos/questions-list.tsx
+++ components/resumos/questions-list.tsx
@@ -14,8 +14,8 @@ export default function QuestionsList({ questoes }: QuestionsListProps) {
         <ScrollArea className="h-[80vh] w-full">
             <main className="flex-grow p-8 transition-all animate-fade-in-down">
                 <ul className="space-y-2">
-                    {questoes.map((questao) => (
-                        <Question questao={questao}/>
+                    {questoes.map((questao, i) => (
+                        <Question key={i} questao={questao} />
                     ))}
                 </ul>
                 <Button disabled className={` transition-all bg-main hover:bg-main hover:opacity-80 float-end m-4 w-48 h-12 font-bold rounded-xl disabled:bg-transparent disabled:border disabled:border-zinc-500 disabled:text-zinc-400 disabled:border-text-z `}>Corrigir <CheckCheck /></Button>
