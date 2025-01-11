68ca2e3ee02df0a60abc4a257ebd48e462e27796
OtavioXavier <otavioxavier.santos@ucsal.edu.br>
2025-01-11 01:08:14 -0300

    :sparkles: feat: correção de questões

--- components/resumos/question.tsx
+++ components/resumos/question.tsx
@@ -1,24 +1,46 @@
+'use client'
+
 import { Questao } from "@/types"
-import { Card, CardTitle } from "../ui/card"
+import { Card, CardFooter, CardTitle } from "../ui/card"
 import { RadioGroup, RadioGroupItem } from "../ui/radio-group"
 import { Label } from "../ui/label"
+import { CheckCheck } from "lucide-react"
+import { josefinSans } from "@/utils/fonts"
+import { useState } from "react"
+import clsx from "clsx"
 
 interface QuestionProps {
-    questao: Questao
+    questao: Questao,
+    corrigir: boolean
 }
 
-export default function Question({ questao }: QuestionProps) {
+export default function Question({ questao, corrigir }: QuestionProps) {
+    const [alternativa, setAlternativa] = useState<string>('');
+
     return (
-        <li key={questao.numero}>
-            <Card className="p-4">
-                <CardTitle>{questao.titulo}</CardTitle>
-                <RadioGroup className="p-4">
+        <li>
+            <Card className="p-4 transition-all">
+                <CardTitle className={`${josefinSans.className}`}>{questao.titulo}</CardTitle>
+                <RadioGroup disabled={corrigir} onValueChange={(value) => setAlternativa(value)} className="p-4 transition-all">
                     {questao.alternativas?.map((alternativa, i) => (
                         <div key={i} className="flex items-center space-x-2">
-                            <RadioGroupItem value={i.toString()} id={`r${i}`} />
-                            <Label htmlFor={`r${i}`}>{alternativa}</Label>
+                            <RadioGroupItem
+                                className="transition-all"
+                                value={alternativa}
+                                id={`r${i}`} />
+
+                            <Label className={`${josefinSans.className}`} htmlFor={`r${i}`}>{alternativa}</Label>
                         </div>
-                    ))}
+                    ))}{corrigir && alternativa !== '' &&
+                        <div className={clsx(
+                            alternativa === questao.resposta ? 'bg-green-500' : 'bg-red-500'
+                            , " bg-opacity-65 rounded-lg h-8 flex justify-center items-center space-x-2 hover:scale-95 transition-all"
+                        )}>
+                            <CheckCheck />
+                            <p className={`${josefinSans.className}`}>
+                                Questão {alternativa === questao.resposta ? 'correta' : 'incorreta'} {questao.resposta}
+                            </p>
+                        </div>}
                 </RadioGroup>
             </Card>
         </li>
--- components/resumos/questions-list.tsx
+++ components/resumos/questions-list.tsx
@@ -1,24 +1,35 @@
+'use client'
+
 import { Questao } from "@/types"
 import { ScrollArea } from "../ui/scroll-area"
 import { Button } from "../ui/button"
-import { CheckCheck } from "lucide-react"
+import { CheckCheck, X } from "lucide-react"
 import Question from "./question"
+import { useState } from "react"
+import clsx from "clsx"
 
 interface QuestionsListProps {
     questoes: Questao[]
 }
 
 export default function QuestionsList({ questoes }: QuestionsListProps) {
+    const [corrigir, setCorrigir] = useState<boolean>(false);
 
     return (
-        <ScrollArea className="h-[80vh] w-full">
+        <ScrollArea className="h-[80vh] w-full transition-all">
             <main className="flex-grow p-8 transition-all animate-fade-in-down">
                 <ul className="space-y-2">
                     {questoes.map((questao, i) => (
-                        <Question key={i} questao={questao} />
+                        <Question key={i} questao={questao} corrigir={corrigir} />
                     ))}
                 </ul>
-                <Button disabled className={` transition-all bg-main hover:bg-main hover:opacity-80 float-end m-4 w-48 h-12 font-bold rounded-xl disabled:bg-transparent disabled:border disabled:border-zinc-500 disabled:text-zinc-400 disabled:border-text-z `}>Corrigir <CheckCheck /></Button>
+                <Button onClick={() => { setCorrigir(!corrigir) }} className={clsx(corrigir ? 'bg-red-500 hover:bg-red-500 ' : 'bg-main hover:bg-main',
+                    `transition-all hover:bg-opacity-80 float-end m-4 w-48 h-12 font-bold rounded-xl disabled:bg-transparent disabled:border disabled:border-zinc-500 disabled:text-zinc-400 disabled:border-text-z `
+                )}>
+                    {corrigir ? 'Cancelar' : 'Corrigir'}
+                    {corrigir ? <X /> : <CheckCheck />}
+
+                </Button>
             </main>
         </ScrollArea>
     )
