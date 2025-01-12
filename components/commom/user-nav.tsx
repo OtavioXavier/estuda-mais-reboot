a98e6e7328ea3bcb31fe631b594e3bf45561d8e8
OtavioXavier <otavioxavier.santos@ucsal.edu.br>
2025-01-12 09:51:48 -0300

    :construction:

--- components/commom/user-nav.tsx
+++ components/commom/user-nav.tsx
@@ -1,92 +1,91 @@
-// "use client";
+"use client";
 
-// import {
-//     Avatar,
-//     AvatarFallback,
-//     AvatarImage,
-// } from "@/components/ui/avatar";
-// import { Button } from "@/components/ui/button";
-// import {
-//     DropdownMenu,
-//     DropdownMenuContent,
-//     DropdownMenuGroup,
-//     DropdownMenuItem,
-//     DropdownMenuLabel,
-//     DropdownMenuSeparator,
-//     DropdownMenuShortcut,
-//     DropdownMenuTrigger,
-// } from "@/components/ui/dropdown-menu";
-// import { createClientComponentClient, User } from "@supabase/auth-helpers-nextjs";
-// import { useRouter } from "next/navigation";
-// import { useCallback, useEffect, useMemo, useState } from "react";
+import {
+    Avatar,
+    AvatarFallback,
+    AvatarImage,
+} from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuShortcut,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { createClientComponentClient, User } from "@supabase/auth-helpers-nextjs";
+import { useRouter } from "next/navigation";
+import { useCallback, useEffect, useMemo, useState } from "react";
 
 export function UserNav() {
 
-    // const [user, setUser] = useState<User | null>(null);
-    // const router = useRouter();
+    const [user, setUser] = useState<User | null>(null);
+    const router = useRouter();
 
-    // const supabase = useMemo(() => createClientComponentClient(), []);
+    const supabase = useMemo(() => createClientComponentClient(), []);
 
-    // const getUser = useCallback(async () => {
-    //     try {
-    //         const {
-    //             data: { user },
-    //             error
-    //         } = await supabase.auth.getUser();
+    const getUser = useCallback(async () => {
+        try {
+            const {
+                data: { user },
+                error
+            } = await supabase.auth.getUser();
+            
+            if (error) {
+                throw new Error(`obtendo o usuario: ${error}`)
+            } else {
+                setUser(user);
+            }
 
-    //         if (error) {
-    //             throw new Error(`obtendo o usuario: ${error}`)
-    //         } else {
-    //             setUser(user);
-    //         }
+        } catch (error) {
+            console.log("ocerreu um erro em user-nav ", error)
+        };
+    }, [supabase])
 
-    //     } catch (error) {
-    //         console.log("ocerreu um erro em user-nav ", error)
-    //     };
-    // }, [supabase])
+    useEffect(() => {
+        getUser();
+    }, [getUser])
 
-    // useEffect(() => {
-    //     getUser();
-    // }, [getUser])
-
-    // const handleSignout = async () => {
-    //     await supabase.auth.signOut();
-    //     router.refresh();
-    // }
+    const handleSignout = async () => {
+        await supabase.auth.signOut();
+        router.refresh();
+    }
 
     return (
-        // <DropdownMenu>
-        //     <DropdownMenuTrigger asChild>
-        //         <Button variant="ghost" className="relative h-8 w-8 rounded-full">
-        //             <Avatar className="h-9 w-9">
-        //                 <AvatarImage src="/avatars/01.jpg" alt="avatar" />
-        //                 <AvatarFallback>SC</AvatarFallback>
-        //             </Avatar>
-        //         </Button>
-        //     </DropdownMenuTrigger>
-        //     <DropdownMenuContent className="w-56" align="end" forceMount>
-        //         <DropdownMenuLabel className="font-normal">
-        //             <div className="flex flex-col space-y-1">
-        //                 <p className="text-sm font-medium leading-none">{user?.email?.split('@')[0]}</p>
-        //                 <p className="text-xs leading-none text-muted-foreground">
-        //                     {user?.email}
-        //                 </p>
-        //             </div>
-        //         </DropdownMenuLabel>
-        //         <DropdownMenuSeparator />
-        //         <DropdownMenuGroup>
-        //             <DropdownMenuItem>
-        //                 Profile
-        //                 <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
-        //             </DropdownMenuItem>
-        //         </DropdownMenuGroup>
-        //         <DropdownMenuSeparator />
-        //         <DropdownMenuItem onClick={handleSignout} className="cursor-pointer">
-        //             Encerrar sessão
-        //             <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
-        //         </DropdownMenuItem>
-        //     </DropdownMenuContent>
-        // </DropdownMenu>
-        <h1>teste</h1>
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button variant="ghost" className="relative h-8 w-8 rounded-full">
+                    <Avatar className="h-9 w-9">
+                        <AvatarImage src="/avatars/01.jpg" alt="avatar" />
+                        <AvatarFallback>SC</AvatarFallback>
+                    </Avatar>
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-56" align="end" forceMount>
+                <DropdownMenuLabel className="font-normal">
+                    <div className="flex flex-col space-y-1">
+                        <p className="text-sm font-medium leading-none">{user?.email?.split('@')[0]}</p>
+                        <p className="text-xs leading-none text-muted-foreground">
+                            {user?.email}
+                        </p>
+                    </div>
+                </DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuGroup>
+                    <DropdownMenuItem>
+                        Profile
+                        <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+                    </DropdownMenuItem>
+                </DropdownMenuGroup>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem onClick={handleSignout} className="cursor-pointer">
+                    Encerrar sessão
+                    <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
     )
 }
