3bbb495f144b9a19aef6b813dffae20221f893b0
OtavioXavier <otavioxavier.santos@ucsal.edu.br>
2025-01-13 23:20:22 -0300

    :bug: fix: barra de scroll dupla

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
     <SummaryProvider>
       <html lang="pt-br">
         <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-hidden`}
         >
           {children}
           <Toaster />
