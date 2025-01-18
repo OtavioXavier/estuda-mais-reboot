b2ad883b11aad506c4526c0ced00fe030eceb899
OtavioXavier <otavioxavier.santos@ucsal.edu.br>
2025-01-18 11:15:21 -0300

    sparkles: feat: tarefas de thread

---
+++ utils/threads/question_thread.mjs
@@ -0,0 +1,35 @@
+'use server'
+
+import { parentPort, threadId } from 'node:worker_threads'
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+if (parentPort) {
+  parentPort.once('message', async ({ assunto }) => {
+    console.time('criando questao-' + threadId)
+
+    const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_GOOGLE_GENERATIVE_AI_API_KEY?.toString() || '');
+    const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash-exp" });
+
+    const prompt = `crie uma questao nova sobre: ${assunto} usando este schema json
+          {
+            questao:
+            id: string,
+            numero(deve ser ${threadId}): number,
+            titulo(Título da questao): string,
+            resposta(alternativa correta): string,
+            alternativas(A, B, C, D): string[]
+          }
+          `;
+
+    const response = await model.generateContent(prompt);
+
+    const responseText = response.response.text();
+    if (!responseText) {
+      throw new Error('A resposta do modelo não contém o texto esperado.');
+    }
+
+    const cleanedText = responseText.replace(/```json|```/g, '').trim();
+    const responseData = JSON.parse(cleanedText);
+    parentPort?.postMessage(responseData.questao)
+  })
+}
