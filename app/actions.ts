218fd37afcf0b1a12110208ebeb8254fa6e47b16
OtavioXavier <otavioxavier.santos@ucsal.edu.br>
2025-01-19 10:19:42 -0300

    :recycle: refactor: removendo comentarios

--- app/actions.ts
+++ app/actions.ts
@@ -1,15 +1,11 @@
 'use server'
-// import { createGoogleGenerativeAI } from '@ai-sdk/google';
-// import { generateObject } from 'ai';
 import { generateSummarySchema, schemaSummaryQuestions } from '@/types/schemas';
 import { IMessageInput, Questao, Resumo, SQ } from '@/types';
 import { Worker } from 'node:worker_threads';
 
 export const generateSummary = async (_: unknown, data: FormData): Promise<SQ | string | null> => {
-  // console.time('generateSummary');
   try {
     const result = generateSummarySchema.safeParse(Object.fromEntries(data));
-    // console.timeLog('generateSummary', 'Após validação inicial');
 
     if (!result.success) {
       return result.error.flatten().fieldErrors.assunto?.[0] ?? 'Erro de validação.';
@@ -40,28 +36,6 @@ export const generateSummary = async (_: unknown, data: FormData): Promise<SQ | 
       resumos,
       questoes
     }
-
-    //     const genAI = createGoogleGenerativeAI({
-    //       apiKey: process.env.NEXT_PUBLIC_GOOGLE_GENERATIVE_AI_API_KEY,
-    //     });
-
-    //     const prompt = `
-    //     Pesquise nos 3 melhores sites de notícias ou artigos científicos sobre: ${assunto}. 
-    //     Resuma cada site com:
-    //       - Nome do site.
-    //       - Título relevante.
-    //       - Resumo do texto (400 palavras).
-    //       - Link para o texto original.
-    //     Gere também 8 a 10 perguntas com respostas, incluindo alternativas (A, B, C, D), todas relacionadas aos resumos.
-    //     Use o schema fornecido para os resultados.
-    // `;
-
-    //     const { object } = await generateObject({
-    //       model: genAI('gemini-2.0-flash-exp'),
-    //       schema: schemaSummaryQuestions,
-    //       prompt,
-    //     });
-    // console.timeLog('generateSummary', 'Após chamada à API externa');
 
     const validatedObject = schemaSummaryQuestions.parse(sq);
     console.timeLog('generateSummary', 'Após validação do schema');
