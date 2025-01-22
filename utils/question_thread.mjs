b6e6dbfa0e2286c9be696ae55af4b2a2d89bf382
OtavioXavier <otavioxavier.santos@ucsal.edu.br>
2025-01-21 22:04:04 -0300

    :bug: fix correção de threads

--- utils/question_thread.mjs
+++ utils/question_thread.mjs
@@ -3,13 +3,13 @@
 import { parentPort, threadId } from 'node:worker_threads'
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-    parentPort?.on('message', async ({ assunto }) => {
-        console.time('criando questao-' + threadId)
+parentPort?.on('message', async ({ assunto }) => {
+  console.time('criando questao-' + threadId)
 
-        const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_GOOGLE_GENERATIVE_AI_API_KEY?.toString() || '');
-        const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash-exp" });
+  const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_GOOGLE_GENERATIVE_AI_API_KEY?.toString() || '');
+  const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash-exp" });
 
-        const prompt = `crie 3 novas questões novas sobre: ${assunto} usando este schema json
+  const prompt = `crie 3 novas questões novas sobre: ${assunto} usando este schema json
           questoes=[{
             questao:
             id: string,
@@ -22,15 +22,15 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
             ]
           `;
 
-        const response = await model.generateContent(prompt);
+  const response = await model.generateContent(prompt);
 
-        const responseText = response.response.text();
+  const responseText = response.response.text();
 
-        if (!responseText) {
-            throw new Error('A resposta do modelo não contém o texto esperado.');
-        }
+  if (!responseText) {
+    throw new Error('A resposta do modelo não contém o texto esperado.');
+  }
 
-        const cleanedText = responseText.replace(/```json|```/g, '').trim();
-        const responseData = JSON.parse(cleanedText);
-        parentPort?.postMessage(responseData.questoes)
-    })
+  const cleanedText = responseText.replace(/```json|```/g, '').trim();
+  const responseData = JSON.parse(cleanedText);
+  parentPort?.postMessage(responseData.questoes)
+})
