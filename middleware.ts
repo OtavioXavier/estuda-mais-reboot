321d05d779b4192e123aeb1c60dab354bfba37a0
OtavioXavier <otavioxavier.santos@ucsal.edu.br>
2025-01-25 22:37:34 -0300

    :sparkles: feat: middleware para autenticação

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,19 @@
-import { NextRequest, NextResponse } from 'next/server'
-import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs';
+import { type NextRequest } from 'next/server'
+import { updateSession } from '@/utils/supabase/middleware'
 
-export async function middleware(req: NextRequest) {
-    // update user's auth session
-    const res = NextResponse.next();
-    try {
-        const supabase = createMiddlewareClient({req, res});
-        await supabase.auth.getSession()
-    } catch (error) {
-        
-    }
-    return res;
+export async function middleware(request: NextRequest) {
+  return await updateSession(request)
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * Feel free to modify this pattern to include more paths.
+     */
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+  ],
 }
