eab2b6308ac1d2d9e80d603886935c8e889c67d8
OtavioXavier <otavioxavier.santos@ucsal.edu.br>
2025-02-07 09:38:36 -0300

    :sparkles: feat: explicação das questões

--- components/resumos/question.tsx
+++ components/resumos/question.tsx
@@ -4,10 +4,13 @@ import { Questao } from "@/types"
 import { Card, CardTitle } from "../ui/card"
 import { RadioGroup, RadioGroupItem } from "../ui/radio-group"
 import { Label } from "../ui/label"
-import { CheckCheck } from "lucide-react"
+import { CheckCheck, LoaderPinwheel } from "lucide-react"
 import { josefinSans } from "@/utils/fonts"
 import { useState } from "react"
 import clsx from "clsx"
+import { Button } from "../ui/button"
+import { generateText } from 'ai';
+import { google } from '../../utils/ai/gemini';
 
 interface QuestionProps {
     questao: Questao,
@@ -17,6 +20,8 @@ interface QuestionProps {
 export default function Question({ questao, corrigir }: QuestionProps) {
     const [alternativa, setAlternativa] = useState<string>('');
     const [feedback, setFeedback] = useState<string>('');
+    const [why, setWhy] = useState<string>('');
+    const [isPending, setIsPending] = useState<boolean>(false);
 
     const handleValueChange = (value: string) => {
         setAlternativa(value);
@@ -24,7 +29,31 @@ export default function Question({ questao, corrigir }: QuestionProps) {
             setFeedback(value === questao.resposta ? 'correta' : 'incorreta');
         }
     };
-    console.log(questao.resposta)
+
+    async function handleWhyQuestion() {
+        try {
+            console.log('Gerando justificativa...')
+            setIsPending(true);
+            const { text } = await generateText({
+                model: google(
+                    "gemini-2.0-flash-exp"
+                ),
+                prompt: `Na questão ${questao.pergunta}, Por que a resposta correta é ${questao.resposta}? responda de forma sucinta e indique um site para pesquisar melhor sobre o assunto`
+            })
+
+            setWhy(text);
+        } catch (error) {
+
+            console.log('Erro durante criação de justificativa: ', error)
+        } finally {
+            setIsPending(false);
+        }
+    }
+
+    if (!corrigir && why !== '') {
+        setWhy('');
+    }
+
     return (
         <li>
             <Card className="p-4 transition-all">
@@ -39,16 +68,37 @@ export default function Question({ questao, corrigir }: QuestionProps) {
 
                             <Label className={`${josefinSans.className}`} htmlFor={`r${i}`}>{alternativa}</Label>
                         </div>
-                    ))}{corrigir && alternativa !== '' &&
-                        <div className={clsx(
-                            alternativa === questao.resposta ? 'bg-green-500' : 'bg-red-500'
-                            , " bg-opacity-65 rounded-lg h-8 flex justify-center items-center space-x-2 hover:scale-95 transition-all"
-                        )}>
-                            <CheckCheck />
-                            <p className={`${josefinSans.className}`}>
-                                Questão {feedback} {feedback === 'incorreta' && `- Resposta correta: ${questao.resposta}`}
-                            </p>
-                        </div>}
+                    ))}
+                    {corrigir && alternativa !== '' &&
+                        <>
+                            <div className="flex justify-between">
+                                <div className={clsx(
+                                    alternativa === questao.resposta ? 'bg-green-500' : 'bg-red-500'
+                                    , "bg-opacity-65 rounded-l-lg h-8 flex justify-center items-center space-x-2 transition-all w-full"
+                                )}>
+                                    <CheckCheck />
+                                    <p className={`${josefinSans.className}`}>
+                                        Questão {feedback} {feedback === 'incorreta' && `- Resposta correta: ${questao.resposta}`}
+                                    </p>
+                                </div>
+                                <Button className="h-8 rounded-l-none rounded-r-lg bg-main hover:bg-blue-800" onClick={() => handleWhyQuestion()}>
+                                    Por quê?
+                                </Button>
+                            </div>
+                            <div className=" text-zinc-500">
+                                {
+                                    why !== '' &&
+                                    <p className={`${josefinSans.className} text-sm`}>{why}</p>
+                                }
+                                {
+                                    isPending &&
+                                    <LoaderPinwheel className="animate-spin" />
+                                }
+                            </div>
+                        </>
+                    }
+
+
                 </RadioGroup>
             </Card>
         </li>
